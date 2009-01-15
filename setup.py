c9d06128d58b888d5b06cd178b55c5e9a37e042c
J. David Ibanez <jdavid@itaapy.com>
2009-01-15 19:27:05 +0100

    Update imports.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Import from itools
-from itools.utils import setup
+from itools.pkg import setup
 
 
 if __name__ == '__main__':
