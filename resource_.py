941a8b341bd72b2d2cba3bc2b6f1a31f3fa26b6e
Hervé Cauwelier <herve@itaapy.com>
2009-04-23 16:11:09 +0200

    resource: fix passing the initial state when making the resource

--- resource_.py
+++ resource_.py
@@ -168,7 +168,7 @@ class DBResource(CatalogAware, IResource):
         if format is None:
             format = cls.class_id
 
-        if issubclass(cls, WorkflowAware):
+        if kw.get('state') is None and issubclass(cls, WorkflowAware):
             schema = cls.get_metadata_schema()
             state = schema['state'].get_default()
             if state is None:
