71ae119c83cb2767b7f86fdbf2e979e14006a8cc
J. David Ibanez <jdavid@itaapy.com>
2009-05-07 19:14:47 +0200

    Add empty test suite.

---
+++ test/test.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+# Copyright (C) 2009 Juan David Ibáñez Palomar <jdavid@itaapy.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Import from the Standard Library
+from unittest import TestLoader, TestSuite, TextTestRunner
+
+# Import tests
+
+
+test_modules = []
+
+
+loader = TestLoader()
+
+if __name__ == '__main__':
+    suite = TestSuite()
+    for module in test_modules:
+        suite.addTest(loader.loadTestsFromModule(module))
+
+    TextTestRunner(verbosity=1).run(suite)
