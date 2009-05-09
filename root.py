5a3f5348538583396f2653a7c037b412645cae79
J. David Ibanez <jdavid@itaapy.com>
2009-05-09 18:29:06 +0200

    Remove useless method.

--- root.py
+++ root.py
@@ -114,12 +114,6 @@ class Root(WebSite):
         return cls(metadata)
 
 
-    @classmethod
-    def get_metadata_schema(cls):
-        schema = WebSite.get_metadata_schema()
-        return schema
-
-
     ########################################################################
     # Override itools.web.root.Root
     ########################################################################
