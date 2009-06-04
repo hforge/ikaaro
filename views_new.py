b0bc1dd2e5abebeba2eade3febd8330ec5023669
J. David Ibanez <jdavid@itaapy.com>
2009-06-04 19:06:20 +0200

    metadata: fix calls to low-level 'set_property'

--- views_new.py
+++ views_new.py
@@ -19,6 +19,7 @@ from urllib import quote
 
 # Import from itools
 from itools.core import freeze
+from itools.csv import Property
 from itools.datatypes import String, Unicode
 from itools.gettext import MSG
 from itools.handlers import checkid
@@ -137,9 +138,9 @@ class NewInstance(AutoForm):
         cls = get_resource_class(class_id)
         child = cls.make_resource(cls, resource, name)
         # The metadata
-        metadata = child.metadata
         language = resource.get_content_language(context)
-        metadata.set_property('title', title, language=language)
+        title = Property(title, lang=language)
+        child.metadata.set_property('title', title)
 
         goto = './%s/' % name
         return context.come_back(messages.MSG_NEW_RESOURCE, goto=goto)
@@ -197,9 +198,9 @@ class ProxyNewInstance(NewInstance):
         cls = get_resource_class(class_id)
         child = cls.make_resource(cls, resource, name)
         # The metadata
-        metadata = child.metadata
         language = resource.get_content_language(context)
-        metadata.set_property('title', title, language=language)
+        title = Property(title, lang=language)
+        child.metadata.set_property('title', title)
 
         goto = './%s/' % name
         return context.come_back(messages.MSG_NEW_RESOURCE, goto=goto)
