47e82bfd7086ed1fdfcaeed51f19d412f74451e0
J. David Ibanez <jdavid@itaapy.com>
2009-07-29 18:52:30 +0200

    Revert "bugzilla#684 fix confirm_registration error"
    This reverts commit ed089e457ff9659b38be268341a428af6a7cdf8f.

--- user_views.py
+++ user_views.py
@@ -41,9 +41,9 @@ class User_ConfirmRegistration(STLForm):
 
     msg = MSG(u'To activate your account, please type a password.')
 
-
-    def GET(self, resource, context):
+    def get_namespace(self, resource, context):
         # Check register key
+        # FIXME This does not work
         must_confirm = resource.get_property('user_must_confirm')
         username = context.get_form_value('username', default='')
         if must_confirm is None:
@@ -52,12 +52,10 @@ class User_ConfirmRegistration(STLForm):
         elif context.get_form_value('key') != must_confirm:
             return context.come_back(messages.MSG_BAD_KEY,
                     goto='/;login?username=%s' % username)
-        return STLForm.GET(self, resource, context)
 
-
-    def get_namespace(self, resource, context):
+        # Ok
         return {
-            'key': resource.get_property('user_must_confirm'),
+            'key': must_confirm,
             'username': resource.get_login_name(),
             'confirmation_msg': self.msg.gettext()}
 
