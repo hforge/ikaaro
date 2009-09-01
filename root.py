043907d8095100378622b287f6f2ce93cf8f014a
Nicolas Deram <nicolas@itaapy.com>
2009-09-01 12:08:29 +0200

    mails: Allow to add an attachment to an email, file handler passed as a new argument "attachment" to send_email.

--- root.py
+++ root.py
@@ -20,7 +20,9 @@
 
 # Import from the Standard Library
 from email.charset import add_charset, add_codec, QP
+from email.mime.application import MIMEApplication
 from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
 from email.MIMEMultipart import MIMEMultipart
 from email.Utils import formatdate
 from email.header import Header
@@ -217,7 +219,7 @@ class Root(WebSite):
     # Email
     def send_email(self, to_addr, subject, from_addr=None, text=None,
                    html=None, encoding='utf-8', subject_with_host=True,
-                   return_receipt=False):
+                   return_receipt=False, attachment=None):
         # Check input data
         if not isinstance(subject, unicode):
             raise TypeError, 'the subject must be a Unicode string'
@@ -274,6 +276,19 @@ class Root(WebSite):
             message.attach(message_html)
         elif text:
             message.attach(message_text)
+        # Attach attachment
+        if attachment:
+            subtype = attachment.get_mimetype()
+            data = attachment.to_str()
+            if subtype[:6] == 'image/':
+                subtype = subtype[6:]
+                mime_cls = MIMEImage
+            else:
+                mime_cls = MIMEApplication
+            message_attachment = mime_cls(data, subtype)
+            message_attachment.add_header('Content-Disposition', 'attachment',
+                                          filename=attachment.name)
+            message.attach(message_attachment)
         # Send email
         server.send_email(message)
 
