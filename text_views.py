331fca308789441be6acab6954ec5804d1321d20
J. David Ibanez <jdavid@itaapy.com>
2009-11-05 15:26:59 +0100

    Fixing csv views

--- text_views.py
+++ text_views.py
@@ -21,6 +21,7 @@ from cgi import escape
 from operator import itemgetter
 
 # Import from itools
+from itools.core import thingy_property, thingy_lazy_property
 from itools.datatypes import Enumerate, Integer, String
 from itools.gettext import MSG
 from itools.web import STLForm, STLView, INFO, ViewField
@@ -29,7 +30,7 @@ from itools.web import STLForm, STLView, INFO, ViewField
 from autoform import AutoForm, get_default_field
 from buttons import RemoveButton
 from file_views import File_Edit
-from forms import TextareaField
+from forms import TextField, TextareaField
 import messages
 from utils import get_parameters
 from views import BrowseForm
@@ -60,7 +61,7 @@ class Text_Edit(File_Edit):
 class Text_View(STLView):
 
     access = 'is_allowed_to_view'
-    title = MSG(u'View')
+    view_title = MSG(u'View')
     icon = 'view.png'
     template = '/ui/text/view.xml'
 
@@ -92,12 +93,11 @@ class Text_ExternalEdit(STLView):
 class PO_Edit(STLForm):
 
     access = 'is_allowed_to_edit'
-    title = MSG(u'Edit')
+    view_title = MSG(u'Edit')
     template = '/ui/PO_edit.xml'
     schema = {
         'msgid': String(mandatory=True),
-        'msgstr': String(mandatory=True),
-    }
+        'msgstr': String(mandatory=True)}
 
 
     def get_namespace(self, resource, context):
@@ -159,38 +159,41 @@ class CSV_View(BrowseForm):
 
     # FIXME We need different permissions for GET and POST
     access = 'is_allowed_to_edit'
-    title = MSG(u'View')
-    schema = {
-        'ids': Integer(mandatory=True, multiple=True),
-    }
+    view_title = MSG(u'View')
+
+    ids = ViewField(datatype=Integer, required=True, multiple=True)
 
 
-    def get_items(self, resource, context):
-        return list(resource.handler.get_rows())
+    @thingy_lazy_property
+    def all_items(self):
+        return list(self.resource.handler.get_rows())
 
 
-    def sort_and_batch(self, resource, context, items):
+    @thingy_lazy_property
+    def items(self):
+        items = self.all_items
+
         # Sort
-        sort_by = context.query['sort_by']
-        reverse = context.query['reverse']
+        sort_by = self.sort_by.value
+        reverse = self.reverse.value
         if sort_by:
             sort_by = int(sort_by)
-            items.sort(key=itemgetter(sort_by), reverse=reverse)
+            items = sorted(items, key=itemgetter(sort_by), reverse=reverse)
 
         # Batch
-        start = context.query['batch_start']
-        size = context.query['batch_size']
+        start = self.batch_start.value
+        size = self.batch_size.value
         return items[start:start+size]
 
 
-    def get_table_columns(self, resource, context):
-        columns = resource.get_columns()
+    def get_table_columns(self):
+        columns = self.resource.get_columns()
         columns.insert(0, ('checkbox', None))
         columns.insert(1, ('index', None))
         return columns
 
 
-    def get_item_value(self, resource, context, item, column):
+    def get_item_value(self, item, column):
         if column == 'checkbox':
             return item.number, False
         elif column == 'index':
@@ -198,7 +201,7 @@ class CSV_View(BrowseForm):
             return index, ';edit_row?index=%s' % index
 
         # A value from the schema
-        handler = resource.handler
+        handler = self.resource.handler
         datatype = handler.get_datatype(column)
         if handler.schema is None:
             value = item[int(column)]
@@ -226,28 +229,42 @@ class RowForm(AutoForm):
 
     access = 'is_allowed_to_edit'
 
-    def get_schema(self, resource, context):
+
+    def get_field_names(self):
+        resource = self.resource
+
         schema = resource.handler.schema
-        if schema is not None:
-            return schema
-        # Default
-        schema = {}
-        for name, title in resource.get_columns():
-            schema[name] = String
-        return schema
+        if schema is None:
+            return [ name for name, title in resource.get_columns() ]
+
+        return schema.keys()
 
 
-    def get_widgets(self, resource, context):
-        schema = self.get_schema(resource, context)
-        return [
-            get_default_field(schema[name])(name, title=title)
-            for name, title in resource.get_columns() ]
+    def get_field(self, name):
+        resource = self.resource
+
+        schema = resource.handler.schema
+        if schema is None:
+            return TextField(name, datatype=String)
+
+        # The title comes from the columns
+        for column_name, column_title in resource.get_columns():
+            if column_name == name:
+                title = column_title
+                break
+        else:
+            title = None
+
+        datatype = schema[name]
+        field = get_default_field(datatype)
+        field = field(name, datatype=datatype, title=title)
+        return field
 
 
 
 class CSV_AddRow(RowForm):
 
-    title = MSG(u'Add Row')
+    view_title = MSG(u'Add Row')
     icon = 'new.png'
     submit_value = MSG(u'Add')
 
@@ -264,24 +281,33 @@ class CSV_AddRow(RowForm):
 
 class CSV_EditRow(RowForm):
 
-    title = MSG(u'Edit row #{id}')
-
     index = ViewField(source='query', datatype=Integer)
 
 
-    def get_title(self, context):
-        id = context.query['index']
-        return self.title.gettext(id=id)
+    def get_field_names(self):
+        field_names = super(CSV_EditRow, self).get_field_names()
+        return ['index'] + field_names
 
 
-    def get_value(self, name):
-        row = self.resource.handler.get_row(self.index.value)
-        return row.get_value(name)
+    @thingy_lazy_property
+    def row(self):
+        return self.resource.handler.get_row(self.index.value)
+
+
+    @thingy_property
+    def view_title(self):
+        title = MSG(u'Edit row #{id}')
+        return title.gettext(id=self.index.value)
+
+
+    def get_value(self, field):
+        return self.row.get_value(field.name)
 
 
     def action(self, resource, context, form):
-        index = context.query['index']
+        index = self.index.value
         resource.handler.update_row(index, **form)
         # Ok
         context.message = messages.MSG_CHANGES_SAVED
+        context.redirect()
 
