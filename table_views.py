8e0e1538f8b8d32433578bbb6380d7c8b12a4adc
Nicolas Deram <nicolas@itaapy.com>
2009-11-12 11:53:24 +0100

    table: fix sort_and_batch on table_views.

--- table_views.py
+++ table_views.py
@@ -85,8 +85,9 @@ class Table_View(SearchForm):
         sort_by = context.query['sort_by']
         reverse = context.query['reverse']
         if sort_by:
-            items.sort(cmp=lambda x,y: cmp(getattr(x, sort_by),
-                       getattr(y, sort_by)), reverse=reverse)
+            get_record_value = resource.handler.get_record_value
+            items.sort(cmp=lambda x,y: cmp(get_record_value(x, sort_by),
+                       get_record_value(y, sort_by)), reverse=reverse)
 
         # Batch
         start = context.query['batch_start']
