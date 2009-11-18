1982523393798c272f5af02c2b08704ab484abac
J. David Ibanez <jdavid@itaapy.com>
2009-11-18 13:00:27 +0100

    Fix adding new files

--- file_views.py
+++ file_views.py
@@ -52,6 +52,8 @@ class File_NewInstance(NewInstance):
     name = None
     file = file_field(required=True, size=35, title=MSG(u'File'))
 
+    field_names = ['file', 'title', 'path']
+
 
     @thingy_lazy_property
     def new_resource_name(self):
@@ -65,16 +67,18 @@ class File_NewInstance(NewInstance):
         if title:
             return title
 
-        filename, mimetype, body = self.file.value
+        filename = self.file.filename
         name, type, language = FileName.decode(filename)
         return name
 
 
     def action(self):
-        filename, mimetype, body = self.file.value
+        filename = self.file.filename
         kk, type, language = FileName.decode(filename)
 
         # Web Pages are first class citizens
+        mimetype = self.file.mimetype
+        body = self.file.body
         if mimetype == 'text/html':
             body = stream_to_str_as_xhtml(HTMLParser(body))
             class_id = 'webpage'
@@ -85,7 +89,6 @@ class File_NewInstance(NewInstance):
         cls = get_resource_class(class_id)
 
         # Multilingual resources, find out the language
-        resource = self.resource
         context = self.context
         if issubclass(cls, Multilingual):
             if language is None:
@@ -102,7 +105,8 @@ class File_NewInstance(NewInstance):
             kw['language'] = language
         else:
             kw['extension'] = type
-        child = resource.make_resource(name, cls, body=body, **kw)
+        container = self.get_container()
+        child = container.make_resource(name, cls, body=body, **kw)
 
         # The title
         title = self.title.value.strip()
