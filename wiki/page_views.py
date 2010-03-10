eb58d326eef35e65a50b2e51c97c0464ad816880
Hervé Cauwelier <herve@itaapy.com>
2010-03-10 12:29:41 +0100

    wiki#838 refactor document tree preparation

--- wiki/page_views.py
+++ wiki/page_views.py
@@ -33,15 +33,15 @@ from docutils.utils import SystemMessage
 from docutils import nodes
 
 # Import from itools
-from itools.core import guess_extension, merge_dicts
+from itools.core import merge_dicts
 from itools.datatypes import String
 from itools.gettext import MSG
-from itools.handlers import checkid, File as FileHandler, ro_database
+from itools.handlers import checkid, ro_database
 from itools.html import XHTMLFile
 from itools.i18n import format_datetime
 from itools.uri import get_reference
 from itools.uri.mailto import Mailto
-from itools.fs import vfs, lfs, FileName
+from itools.fs import lfs, FileName
 from itools.web import BaseView, STLView, ERROR
 from itools.xml import XMLParser, XMLError
 
@@ -52,6 +52,62 @@ from ikaaro.resource_views import DBResource_Edit
 
 
 figure_style_converter = compile(r'\\begin\{figure\}\[.*?\]')
+
+
+def is_external(reference):
+    return (type(reference) is Mailto or reference.scheme
+            or reference.authority)
+
+
+
+def default_reference_resolver(resource, reference, context):
+    """Make canonical URI to the website for future download.
+    """
+    # Resolve against the parent
+    uri = context.uri.resolve('..')
+    return str(uri.resolve2(reference))
+
+
+
+def resolve_references(doctree, resource, context,
+        reference_resolver=default_reference_resolver):
+    """Translate resource path to handler uri.
+    """
+    for node in doctree.traverse(condition=nodes.reference):
+        wiki_name = node.get('wiki_name')
+        if wiki_name is False:
+            # Broken link
+            continue
+        elif wiki_name:
+            # Wiki link
+            reference = get_reference(wiki_name)
+            node['refuri'] = reference_resolver(resource, reference, context)
+        elif wiki_name is None:
+            # Regular link: point back to the site
+            refuri = node.get('refuri')
+            if refuri is None:
+                continue
+            reference = get_reference(refuri.encode('utf_8'))
+            if is_external(reference):
+                # Keep the unicode version
+                continue
+            node['refuri'] = reference_resolver(resource, reference, context)
+
+
+
+def resolve_images(doctree, resource, context):
+    """Translate image path to handler uri.
+    """
+    for node in doctree.traverse(condition=nodes.image):
+        reference = get_reference(node['uri'].encode('utf8'))
+        if is_external(reference):
+            continue
+        name = str(reference.path)
+        image = resource.get_resource(name, soft=True)
+        if image is not None:
+            node['uri'] = image.handler.key
+
+
 
 class WikiPage_View(BaseView):
 
@@ -65,50 +121,67 @@ class WikiPage_View(BaseView):
         parent = resource.parent
 
         try:
-            document = resource.get_document()
+            doctree = resource.get_doctree()
         except SystemMessage, e:
             # Critical
-            context.message = ERROR(u'Syntax error: {error}', error=e.message)
+            context.message = ERROR(u'Syntax error: {error}',
+                    error=e.message)
             return XMLParser('<pre>' + resource.handler.to_str() + '</pre>')
 
         # Decorate the links and resolve them against the published resource
-        for node in document.traverse(condition=nodes.reference):
+        for node in doctree.traverse(condition=nodes.reference):
             refname = node.get('wiki_name')
             if refname is None:
                 # Regular link
                 if node.get('refid'):
                     node['classes'].append('internal')
                 refuri = node.get('refuri')
-                if refuri is not None:
-                    reference = get_reference(refuri.encode('utf_8'))
-                    # Skip external
-                    if reference.scheme or reference.authority:
-                        node['classes'].append('external')
-                        continue
-                    destination = resource.get_resource(reference.path, soft=True)
-                    if destination is not None:
-                        node['refuri'] = context.get_link(destination)
+                if refuri is None:
+                    continue
+                reference = get_reference(refuri.encode('utf_8'))
+                # Skip external
+                if is_external(reference):
+                    node['classes'].append('external')
+                    continue
+                destination = resource.get_resource(reference.path,
+                        soft=True)
+                if destination is None:
+                    continue
+                refuri = context.get_link(destination)
+                if reference.fragment:
+                    refuri = "%s#%s" % (refuri, reference.fragment)
+                node['refuri'] = refuri
             elif refname is False:
-                    # Wiki link not found
-                    node['classes'].append('nowiki')
-                    prefix = resource.get_pathto(parent)
-                    title = node['name']
-                    title_encoded = title.encode('utf_8')
-                    params = {'type': resource.__class__.__name__,
-                              'title': title_encoded,
-                              'name': checkid(title) or title_encoded}
-                    refuri = "%s/;new_resource?%s" % (prefix,
-                                                      urlencode(params))
-                    node['refuri'] = refuri
+                # Wiki link not found
+                node['classes'].append('nowiki')
+                prefix = resource.get_pathto(parent)
+                title = node['name']
+                title_encoded = title.encode('utf_8')
+                params = {'type': resource.__class__.__name__,
+                          'title': title_encoded,
+                          'name': checkid(title) or title_encoded}
+                refuri = "%s/;new_resource?%s" % (prefix,
+                                                  urlencode(params))
+                node['refuri'] = refuri
             else:
                 # Wiki link found, "refname" is the path
                 node['classes'].append('wiki')
                 destination = resource.get_resource(refname)
                 node['refuri'] = context.get_link(destination)
 
+        # Download images directly
+        for node in doctree.traverse(condition=nodes.image):
+            reference = get_reference(node['uri'].encode('utf_8'))
+            if is_external(reference):
+                continue
+            destination = resource.get_resource(reference.path, soft=True)
+            if destination is None:
+                continue
+            node['uri'] = '%s/;download' % context.get_link(destination)
+
         # Manipulate publisher directly (from publish_from_doctree)
         reader = Reader(parser_name='null')
-        pub = Publisher(reader, None, None, source=DocTreeInput(document),
+        pub = Publisher(reader, None, None, source=DocTreeInput(doctree),
                 destination_class=StringOutput)
         pub.set_writer('html')
         pub.process_programmatic_settings(None, resource.overrides, None)
@@ -136,85 +209,15 @@ class WikiPage_ToPDF(BaseView):
                     u"the server.")
             return context.come_back(msg)
 
-        parent = resource.parent
-
-        document = resource.get_document()
+        doctree = resource.get_doctree()
         # We hack a bit the document tree to enhance the PDF produced
-        for node in document.traverse(condition=nodes.reference):
-            refname = node.get('wiki_name')
-            if refname is None:
-                # Regular link: point back to the site
-                refuri = node.get('refuri')
-                if refuri is not None:
-                    reference = get_reference(refuri.encode('utf_8'))
-                    if isinstance(reference, Mailto):
-                        # mailto:
-                        node['refuri'] = str(reference)
-                    else:
-                        # Make canonical URI to the website for future
-                        # download
-                        node['refuri'] = str(context.uri.resolve(reference))
-                continue
-            # Now consider the link is valid
-            title = node['name']
-            document.nameids[title.lower()] = checkid(title)
-            # The journey ends here for broken links
-            if refname is False:
-                continue
-            # We extend the page with links to files
-            destination = resource.get_resource(refname)
-            # Set the URI to download it
-            prefix = context.resource.get_pathto(destination)
-            node['refuri'] = str(context.uri.resolve(prefix))
-            continue
-
-        # Find the list of images to append
-        images = []
-        for node in document.traverse(condition=nodes.image):
-            uri = node['uri'].encode('utf_8')
-            # Hack to handle local images
-            reference = get_reference(uri)
-            if not reference.scheme and uri.endswith('/;download'):
-                reference = get_reference(uri[:-len('/;download')])
-            if reference.scheme or reference.authority:
-                # Fetch external image
-                try:
-                    image = ro_database.get_handler(reference)
-                except LookupError:
-                    image = None
-                else:
-                    filename = reference.path.get_name()
-                    name, ext, lang = FileName.decode(filename)
-                    # At least images from ikaaro won't have an extension
-                    if ext is None:
-                        reference = str(reference)
-                        mimetype = vfs.get_mimetype(reference)
-                        ext = guess_extension(mimetype)[1:]
-                        filename = FileName.encode((name, ext, lang))
-            else:
-                image = parent.get_resource(reference.path, soft=True)
-                if image is not None:
-                    filename = image.get_property('filename')
-            if image is None:
-                # Missing image, prevent pdfLaTeX failure
-                image = resource.get_resource('/ui/wiki/missing.png')
-                filename = image.name
-                # Remove all path so the image is found in tempdir
-                node['uri'] = filename
-                images.append((image, filename))
-            else:
-                # pdflatex does not support the ".jpeg" extension
-                name, ext, lang = FileName.decode(filename)
-                if ext == 'jpeg':
-                    filename = FileName.encode((name, 'jpg', lang))
-                # Remove all path so the image is found in tempdir
-                node['uri'] = filename
-                images.append((image, filename))
+        resolve_references(doctree, resource, context)
+        resolve_images(doctree, resource, context)
 
         # Make some modifications
         overrides = dict(resource.overrides)
         overrides['stylesheet'] = 'style.tex'
-        output = publish_from_doctree(document, writer_name='latex',
+        output = publish_from_doctree(doctree, writer_name='latex',
                                       settings_overrides=overrides)
         output = figure_style_converter.sub(r'\\begin{figure}[H]', output)
 
@@ -234,7 +237,6 @@ class WikiPage_ToPDF(BaseView):
             stylesheet.save_state_to_file(file)
         finally:
             file.close()
-
         # The 'powered' image...
         image = resource.get_resource('/ui/aruni/images/ikaaro_powered.png')
         file = tempdir.make_file('ikaaro.png')
@@ -242,24 +244,34 @@ class WikiPage_ToPDF(BaseView):
             image.save_state_to_file(file)
         finally:
             file.close()
-
+        # The stub for not found images
+        stub = resource.get_resource('/ui/wiki/missing.png')
         # And referenced images
-        for image, filename in images:
+        for node in doctree.traverse(condition=nodes.image):
+            uri = node['uri']
+            filename = uri.rsplit('/', 1)[-1]
+            # pdflatex does not support the ".jpeg" extension
+            name, ext, lang = FileName.decode(filename)
+            if ext == 'jpeg':
+                filename = FileName.encode((name, 'jpg', lang))
             if tempdir.exists(filename):
                 continue
             file = tempdir.make_file(filename)
             try:
-                if isinstance(image, FileHandler):
-                    try:
-                        image.save_state_to_file(file)
-                    except XMLError:
-                        # XMLError is raised by unexpected HTTP responses
-                        # from external images. See bug #249
-                        pass
-                else:
-                    image.handler.save_state_to_file(file)
+                try:
+                    handler = ro_database.get_handler(uri)
+                except LookupError:
+                    handler = stub
+                try:
+                    handler.save_state_to_file(file)
+                except XMLError:
+                    # XMLError is raised by unexpected HTTP responses
+                    # from external images. See bug #249
+                    pass
             finally:
                 file.close()
+            # Remove all path so the image is found in tempdir
+            node['uri'] = filename
 
         # From LaTeX to PDF
         command = ['pdflatex', '-8bit', '-no-file-line-error',
