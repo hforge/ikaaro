d20eca6646280296f0a7200623966f20b9df33d5
J. David Ibanez <jdavid@itaapy.com>
2010-03-31 14:56:47 +0200

    Refactor code in "Folder.move_resource"

--- folder.py
+++ folder.py
@@ -202,16 +202,12 @@ class Folder(DBResource):
 
 
     def move_resource(self, source_path, target_path):
-        # Cannot move a resource to a subdirectory of itself
-        abspath = self.get_canonical_path()
-        if source_path == abspath:
-            message = 'cannot move a resource to a subdirectory of itself'
-            raise ConsistencyError, message
-
         # Find out the source and target absolute URIs
         source_path, target_path = self._resolve_source_target(source_path,
                                                                target_path)
 
+        # Cannot move a resource to a subdirectory of itself
+        abspath = self.get_canonical_path()
         if source_path.get_prefix(abspath) == source_path:
             message = 'cannot move a resource to a subdirectory of itself'
             raise ConsistencyError, message
