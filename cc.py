a668aff18785ade2ada09fe8ca316c2858e6ffd7
David Versmisse <david.versmisse@itaapy.com>
2011-02-03 12:02:48 +0100

    cc#1165: now we have the possibility to add new users directly in the subscriptions form

--- cc.py
+++ cc.py
@@ -18,12 +18,13 @@
 from operator import itemgetter
 
 # Import from itools
-from itools.datatypes import Enumerate, Tokens
+from itools.datatypes import Enumerate, Tokens, Email
 from itools.gettext import MSG
 from itools.web import STLForm, ERROR
 
 # Import from ikaaro
 from messages import MSG_CHANGES_SAVED
+
 
 
 class UsersList(Enumerate):
@@ -32,6 +33,7 @@ class UsersList(Enumerate):
 
     def get_options(self):
         site_root = self.resource.get_site_root()
+
         # Members
         included_roles = self.included_roles
         if included_roles:
@@ -56,8 +58,22 @@ class UsersList(Enumerate):
                 continue
             value = user.get_title()
             options.append({'name': name, 'value': value})
+
         options.sort(key=itemgetter('value'))
         return options
+
+
+
+class UsersRawList(Tokens):
+
+    @staticmethod
+    def is_valid(value):
+        if not value:
+            return True
+        for email in value:
+            if not Email.is_valid(email):
+                return False
+        return True
 
 
 
@@ -67,12 +83,22 @@ class SubscribeForm(STLForm):
     title = MSG(u'Subscriptions')
     template = '/ui/subscribe.xml'
 
+
     def get_schema(self, resource, context):
-        return {'cc_list': UsersList(resource=resource, multiple=True)}
+        return {'cc_list': UsersList(resource=resource, multiple=True),
+                'new_users': UsersRawList()}
 
 
     def get_namespace(self, resource, context):
-        cc = resource.get_property('cc_list')
+        namespace = super(SubscribeForm, self).get_namespace(resource, context)
+
+        # Get the good cc_list
+        submit = (context.method == 'POST')
+        if submit:
+            cc_list = context.get_form_value('cc_list',
+                      type=UsersList(resource=resource, multiple=True))
+        else:
+            cc_list = resource.get_property('cc_list')
 
         # Admin
         ac = resource.get_access_control()
@@ -81,40 +107,71 @@ class SubscribeForm(STLForm):
         # Current user
         user = context.user
         if user:
-            username = user.name
-            user_title = user.get_title()
-            user_checked = username in cc
+            # Find the user
+            for an_user in namespace['cc_list']['value']:
+                if an_user['name'] == user.name:
+                    current_user = an_user
+                    current_user['selected'] = user.name in cc_list
+                    break
         else:
-            username = user_title = user_checked = None
+            current_user = None
 
         # Other users
-        root = context.root
-        site_root = resource.get_site_root()
         subscribed = []
         not_subscribed = []
-        for name in site_root.get_members():
-            if name == username:
+        for an_user in namespace['cc_list']['value']:
+            if an_user['name'] == user.name:
                 continue
-            user = {'name': name, 'title': root.get_user_title(name)}
-            if name in cc:
-                subscribed.append(user)
+            if an_user['name'] in cc_list:
+                an_user['selected'] = True
+                subscribed.append(an_user)
             else:
-                not_subscribed.append(user)
-        subscribed.sort(key=itemgetter('title'))
-        not_subscribed.sort(key=itemgetter('title'))
+                an_user['selected'] = False
+                not_subscribed.append(an_user)
+        subscribed.sort(key=itemgetter('value'))
+        not_subscribed.sort(key=itemgetter('value'))
 
         # Ok
-        return {
-            'is_admin': is_admin,
-            'user_name': username,
-            'user_title': user_title,
-            'user_checked': user_checked,
-            'subscribed': subscribed,
-            'not_subscribed': not_subscribed}
+        return {'current_user': current_user,
+                'is_admin': is_admin,
+                'subscribed': subscribed,
+                'not_subscribed': not_subscribed,
+                'new_users': namespace['new_users']}
+
+
+    def _add_user(self, resource, context, email):
+        root = context.root
+        site_root = resource.get_site_root()
+        users = root.get_resource('users')
+
+        # Check whether the user already exists
+        results = root.search(email=email)
+        if len(results):
+            user_id = results.get_documents()[0].name
+        else:
+            user_id = None
+
+        # Get the user (create it if needed)
+        if user_id is None:
+            # Add the user
+            user = users.set_user(email, password=None)
+            user_id = user.name
+            user.send_confirmation(context, email)
+        else:
+            # Check the user is not yet in the group
+            if user_id in site_root.get_members():
+                return user_id
+            user = users.get_resource(user_id)
+            user.send_registration(context, email)
+
+        # Set the role
+        site_root.set_user_role(user_id, role='guests')
+        return user_id
 
 
     def action(self, resource, context, form):
         new_cc = form.get('cc_list')
+        new_users = form.get('new_users')
 
         # Case 1: anonymous user, not yet supported
         user = context.user
@@ -126,21 +183,28 @@ class SubscribeForm(STLForm):
         ac = resource.get_access_control()
         is_admin = ac.is_admin(context.user, resource)
         if is_admin:
+            new_id_set = set()
+            for email in new_users:
+                new_id_set.add(self._add_user(resource, context, email))
+            new_cc = set(new_cc).union(new_id_set)
             resource.set_property('cc_list', tuple(new_cc))
             context.message = MSG_CHANGES_SAVED
+            context.get_form()['cc_list'] = list(new_cc)
+            context.get_form()['new_users'] = ''
             return
 
         # Case 3: someone else
         old_cc = resource.get_property('cc_list')
         if user.name in new_cc:
-            new_cc = list(old_cc)
-            new_cc.append(user.name)
+            new_cc = set(old_cc)
+            new_cc.add(user.name)
         else:
-            new_cc = list(old_cc)
-            new_cc.remove(user.name)
-
+            new_cc = set(old_cc)
+            new_cc.discard(user.name)
         resource.set_property('cc_list', tuple(new_cc))
         context.message = MSG_CHANGES_SAVED
+        context.get_form()['cc_list'] = list(new_cc)
+        context.get_form()['new_users'] = ''
 
 
 
