957ed0a2edcc5976abba36df722d82029287ebc6
J. David Ibañez <jdavid@itaapy.com>
2011-02-17 18:36:50 +0100

    bug#994: when a user makes a website make him admin

--- website_views.py
+++ website_views.py
@@ -223,6 +223,8 @@ class WebSite_NewInstance(NewInstance):
         vhosts = [ x.strip() for x in vhosts.splitlines() ]
         vhosts = [ x for x in vhosts if x ]
         child.metadata.set_property('vhosts', vhosts)
+        # Add initial user
+        child.set_user_role(context.user.name, 'admins')
         # Ok
         goto = str(resource.get_pathto(child))
         return context.come_back(MSG_NEW_RESOURCE, goto=goto)
