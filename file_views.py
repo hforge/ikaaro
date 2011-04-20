202ffdadf5755fc3ed436adc10c770634cc127e0
J. David Ibáñez <jdavid@itaapy.com>
2011-04-20 17:34:20 +0200

    bug#1099: update to changes in the 'get_thumbnail'

--- file_views.py
+++ file_views.py
@@ -286,7 +286,7 @@ class Image_Thumbnail(BaseView):
     query_schema = {
         'width': Integer(default=48),
         'height': Integer(default=48),
-        'strict': Boolean(default=False)}
+        'fit': Boolean(default=False)}
 
     def get_mtime(self, resource):
         return resource.handler.get_mtime()
@@ -297,11 +297,11 @@ class Image_Thumbnail(BaseView):
         image_width, image_height = handler.get_size()
         width = context.query['width'] or image_width
         height = context.query['height'] or image_height
-        strict = context.query['strict']
+        fit = context.query['fit']
 
         # TODO generate the thumbnail in the resource format
         format = 'png' if resource.metadata.format == 'image/png' else 'jpeg'
-        data, format = handler.get_thumbnail(width, height, format, strict)
+        data, format = handler.get_thumbnail(width, height, format, fit)
         if data is None:
             default = resource.get_resource('/ui/icons/48x48/image.png')
             data = default.to_str()
