6e64fe13539258ee4f7a7ca6be9d861608493e32
Taverne Sylvain <sylvain@agicia.com>
2017-04-26 16:07:10 +0200

    Tests: Allow to use junit & use exit code

--- test/test.py
+++ test/test.py
@@ -14,23 +14,44 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 # Import from the Standard Library
+from optparse import OptionParser
+from sys import exit
 from unittest import TestLoader, TestSuite, TextTestRunner
 
+# Import from itools
+from itools.core import get_abspath
+
 # Import tests
-import test_database
 import test_metadata
 import test_server
+from junitxml import JUnitXmlResult
 
 
-test_modules = [test_database, test_metadata, test_server]
+test_modules = [test_metadata, test_server]
 
 
 loader = TestLoader()
 
 if __name__ == '__main__':
+    usage = '%prog [OPTIONS]'
+    description = 'Run ikaaro tests'
+    parser = OptionParser(usage, description=description)
+    parser.add_option('-m', '--mode', default='standard', help='tests mode')
+    options, args = parser.parse_args()
     suite = TestSuite()
     for module in test_modules:
         suite.addTest(loader.loadTestsFromModule(module))
-
-    TextTestRunner(verbosity=1).run(suite)
+    if options.mode == 'standard':
+        ret = TextTestRunner(verbosity=1).run(suite)
+    elif options.mode == 'junitxml':
+        path = get_abspath('./junit.xml')
+        print('Result is here: %s' % path)
+        f = file(path, 'wb')
+        result = JUnitXmlResult(f)
+        result.startTestRun()
+        ret = suite.run(result)
+        result.stopTestRun()
+    exit_code = not ret.wasSuccessful()
+    exit(exit_code)
