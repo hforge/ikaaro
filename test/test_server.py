48384e41772772e3c6927bf75b805ff61f07ccb2
Taverne Sylvain <sylvain@agicia.com>
2017-09-10 20:55:58 +0200

    Tests: Fix server tests

--- test/test_server.py
+++ test/test_server.py
@@ -85,42 +85,39 @@ class TestJsonAction_View(BaseView):
 
 class ServerTestCase(TestCase):
 
+
     def setUp(self):
-        global SERVER
-        if SERVER is not None:
-            return
         self.tearDown()
-        path = DATABASE_TEST_PATH
-        paths = [DATABASE_TEST_PATH]
+        create_server('test_server', 'test@hforge.org',
+            'password', 'ikaaro', website_languages=['en', 'fr'])
+
+
+    def tearDown(self):
+        paths = ['test_server']
         for path in paths:
             if lfs.exists(path):
                 lfs.remove(path)
-        email = 'test@example.com'
-        password = 'password'
-        root = None
-        modules = []
-        listen_port = 8081
-        create_server(path, email, password, root,  modules, listen_port)
-        server = Server(path)
-        server.start(detach=False, loop=False)
-        SERVER = server
+
+
+    def get_server(self):
+        return Server('test_server')
 
 
     def test_server_ctrl(self):
-        server = SERVER
+        server = self.get_server()
         # Test /;ctrl view
         retour = server.do_request('GET', '/;_ctrl')
         self.assertEqual(retour['status'], 200)
 
 
     def test_server_up(self):
-        server = SERVER
+        server = self.get_server()
         retour = server.do_request('GET', '/api/status', as_json=True)
         self.assertEqual(retour['entity']['up'], True)
 
 
     def test_server_404(self):
-        server = SERVER
+        server = self.get_server()
         # Error 404 json
         retour = server.do_request('GET', '/api/404', as_json=True)
         self.assertEqual(retour['status'], 404)
@@ -134,7 +131,7 @@ class ServerTestCase(TestCase):
 
     # FIXME
     #def test_server_forbidden(self):
-    #    server = SERVER
+    #    server = self.get_server()
     #    server.dispatcher.add('/test/forbidden', TestPlainText_View(access=False))
     #    user = server.root.get_resource('/users/0')
     #    retour = server.do_request('GET', '/test/forbidden',
@@ -143,14 +140,14 @@ class ServerTestCase(TestCase):
 
 
     def test_server_unauthorized(self):
-        server = SERVER
+        server = self.get_server()
         server.dispatcher.add('/test/unauthorized', TestPlainText_View(access=False))
         retour = server.do_request('GET', '/test/unauthorized')
         self.assertEqual(retour['status'], 401)
 
 
     def test_html(self):
-        server = SERVER
+        server = self.get_server()
         server.dispatcher.add('/test/html', TestHTML_View)
         retour = server.do_request('GET', '/test/html')
         self.assertEqual(retour['status'], 200)
@@ -158,7 +155,7 @@ class ServerTestCase(TestCase):
 
 
     def test_plain_text(self):
-        server = SERVER
+        server = self.get_server()
         server.dispatcher.add('/test/text', TestPlainText_View)
         retour = server.do_request('GET', '/test/text')
         self.assertEqual(retour['status'], 200)
@@ -166,7 +163,7 @@ class ServerTestCase(TestCase):
 
 
     def test_json(self):
-        server = SERVER
+        server = self.get_server()
         server.dispatcher.add('/test/json', TestJson_View)
         retour = server.do_request('GET', '/test/json?name=world', as_json=True)
         self.assertEqual(retour['status'], 200)
@@ -174,7 +171,7 @@ class ServerTestCase(TestCase):
 
 
     def test_action(self):
-        server = SERVER
+        server = self.get_server()
         server.dispatcher.add('/test/json-action', TestJsonAction_View)
         body = {'action': 'hello', 'name': 'world'}
         retour = server.do_request('POST', '/test/json-action', body=body, as_json=True)
@@ -185,7 +182,7 @@ class ServerTestCase(TestCase):
 
 
     def test_commit(self):
-        server = SERVER
+        server = self.get_server()
         server.dispatcher.add('/test/json-action', TestJsonAction_View)
         body = {'action': 'set_root_title', 'title': u'Sylvain'}
         retour = server.do_request('POST', '/test/json-action', body=body, as_json=True)
@@ -200,7 +197,7 @@ class ServerTestCase(TestCase):
 
 
     def test_stop_server(self):
-        server = SERVER
+        server = self.get_server()
         server.stop()
         # Remove database
         path = DATABASE_TEST_PATH
