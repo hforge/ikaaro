2255469c69bcc50f61291bd3f7208522d73067c6
Taverne Sylvain <sylvain@agicia.com>
2017-09-19 01:19:58 +0200

    Context: Reorganize code

--- ikaaro/context.py
+++ ikaaro/context.py
@@ -19,14 +19,15 @@ from base64 import decodestring, encodestring
 from copy import deepcopy
 from datetime import datetime, timedelta
 import json
-from pytz import timezone
 from hashlib import sha224
+from pytz import timezone
 from urllib import quote, unquote
 
 # Import from itools
 from itools.core import freeze, proto_lazy_property
 from itools.core import fixed_offset, is_prototype, local_tz
 from itools.core import prototype
+from itools.datatypes import String, HTTPDate
 from itools.fs import lfs
 from itools.handlers import ro_database
 from itools.i18n import has_language
@@ -35,7 +36,6 @@ from itools.i18n import AcceptLanguageType, format_number
 from itools.log import log_warning
 from itools.uri import normalize_path
 from itools.uri import decode_query, get_reference, Path, Reference
-from itools.datatypes import String, HTTPDate
 from itools.web.context import get_form_value
 from itools.web import ERROR
 from itools.web.headers import get_type, Cookie, SetCookieDataType
@@ -74,79 +74,6 @@ class CMSContext(prototype):
     uri = None
     user = None
     view = None
-
-    #######################################################################
-    # Root
-    #######################################################################
-
-    @proto_lazy_property
-    def root(self):
-        return self.database.get_resource('/')
-
-
-    def find_site_root(self):
-        self.site_root = self.root
-
-    #######################################################################
-    # Templates API
-    #######################################################################
-
-    def get_template(self, web_path):
-        warning = None
-        web_path_object = Path(normalize_path(web_path))
-        skin_name = web_path_object[1]
-        try:
-            skin = skin_registry[skin_name]
-        except KeyError:
-            warning = 'WARNING: web_path {} is obsolete use /ui/ikaaro/'
-            warning = warning.format(web_path)
-            web_path = web_path.replace('/ui/', '/ui/ikaaro/')
-            skin = skin_registry['ikaaro']
-        # 1) Try with envionment skin
-        skin_key = skin.get_environment_key(self.server)
-        web_path = web_path.replace(skin.base_path, '')
-        template = self.get_template_from_skin_key(skin_key, web_path, warning)
-        if template:
-            return template
-        # 2) Try with standard skin
-        return self.get_template_from_skin_key(skin.key, web_path, warning)
-
-
-    def get_template_from_skin_key(self, skin_key, web_path, warning):
-        local_path = skin_key + web_path
-        # 3. Get the handler
-        handler = ro_database.get_handler(local_path, soft=True)
-        if handler:
-            if warning:
-                print warning
-            return handler
-
-        # 4. Not an exact match: trigger language negotiation
-        folder_path, name = local_path.rsplit('/', 1)
-        name = name + '.'
-        n = len(name)
-        languages = []
-        for x in lfs.get_names(folder_path):
-            if x[:n] == name:
-                language = x[n:]
-                if has_language(language):
-                    languages.append(language)
-        if not languages:
-            return None
-
-        # 4.1 Get the best variant
-        accept = self.accept_language
-        language = accept.select_language(languages)
-        # Print Warning
-        if warning:
-            print warning
-        # 4.2 By default use whatever variant
-        # (XXX we need a way to define the default)
-        if language is None:
-            language = languages[0]
-        local_path = '%s.%s' % (local_path, language)
-        return ro_database.get_handler(local_path)
-
 
     #######################################################################
     # WSGI environ
@@ -231,6 +158,79 @@ class CMSContext(prototype):
     @proto_lazy_property
     def timestamp(self):
         return datetime.utcnow().replace(tzinfo=fixed_offset(0))
+
+
+    #######################################################################
+    # Root
+    #######################################################################
+
+    @proto_lazy_property
+    def root(self):
+        return self.database.get_resource('/')
+
+
+    def find_site_root(self):
+        self.site_root = self.root
+
+    #######################################################################
+    # Templates API
+    #######################################################################
+
+    def get_template(self, web_path):
+        warning = None
+        web_path_object = Path(normalize_path(web_path))
+        skin_name = web_path_object[1]
+        try:
+            skin = skin_registry[skin_name]
+        except KeyError:
+            warning = 'WARNING: web_path {} is obsolete use /ui/ikaaro/'
+            warning = warning.format(web_path)
+            web_path = web_path.replace('/ui/', '/ui/ikaaro/')
+            skin = skin_registry['ikaaro']
+        # 1) Try with envionment skin
+        skin_key = skin.get_environment_key(self.server)
+        web_path = web_path.replace(skin.base_path, '')
+        template = self.get_template_from_skin_key(skin_key, web_path, warning)
+        if template:
+            return template
+        # 2) Try with standard skin
+        return self.get_template_from_skin_key(skin.key, web_path, warning)
+
+
+    def get_template_from_skin_key(self, skin_key, web_path, warning):
+        local_path = skin_key + web_path
+        # 3. Get the handler
+        handler = ro_database.get_handler(local_path, soft=True)
+        if handler:
+            if warning:
+                print warning
+            return handler
+
+        # 4. Not an exact match: trigger language negotiation
+        folder_path, name = local_path.rsplit('/', 1)
+        name = name + '.'
+        n = len(name)
+        languages = []
+        for x in lfs.get_names(folder_path):
+            if x[:n] == name:
+                language = x[n:]
+                if has_language(language):
+                    languages.append(language)
+        if not languages:
+            return None
+
+        # 4.1 Get the best variant
+        accept = self.accept_language
+        language = accept.select_language(languages)
+        # Print Warning
+        if warning:
+            print warning
+        # 4.2 By default use whatever variant
+        # (XXX we need a way to define the default)
+        if language is None:
+            language = languages[0]
+        local_path = '%s.%s' % (local_path, language)
+        return ro_database.get_handler(local_path)
 
 
     #######################################################################
