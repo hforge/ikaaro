5bbed05585a1bcf7020979c242b8a6853646ff68
mathieu.pequin <mathieu.pequin@bepatient.com>
2022-05-06 11:48:39 +0200

    jwcrypto==1.2

--- ikaaro/__init__.py
+++ ikaaro/__init__.py
@@ -55,7 +55,7 @@ for name, import_path, reason in [
 
 
 # The version
-__version__ = "0.80.5"
+__version__ = "0.80.6"
 __version_info__ = tuple(
     int(num) if num.isdigit() else num
     for num in __version__.replace("-", ".", 1).split(".")
